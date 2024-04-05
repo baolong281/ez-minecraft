2b673715808d33682edf179da469d5360e284a8c
dylan h <baolong281@gmail.com>
2024-04-05 11:57:12 -0500

    forgot to save lol

--- src/vm.ts
+++ src/vm.ts
@@ -30,6 +30,21 @@ class VMInstance {
     const end = performance.now();
     return Math.round(((end - start) / 1000) * 10) / 10;
   }
+
+  public async stopServer() {
+    const res = await getServerResponse();
+    if (!res.online) {
+      return -1;
+    }
+    const start = performance.now();
+    const result = await this.client.virtualMachines.beginPowerOffAndWait(
+      config.R_GROUP,
+      this.vmname,
+    );
+    logger.info("stop_server", result);
+    const end = performance.now();
+    return Math.round(((end - start) / 1000) * 10) / 10;
+  }
 }
 
 export const VM = new VMInstance();
